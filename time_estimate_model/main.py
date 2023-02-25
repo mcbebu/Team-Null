9da0a7128774f05cd8fbd53983459bcceacb973d
Guo,Ziyi <59839293+ainu412@users.noreply.github.com>
2023-02-25 20:55:07 +0800

    knn regressor on solely source and destination longitude & latitude perform badly

--- time_estimate_model/main.py
+++ time_estimate_model/main.py
@@ -15,6 +15,7 @@ from sklearn.neighbors import KNeighborsRegressor
 
 # input: source_longitude, source_latitude, destination_longitude, destination_latitude
 # output: estimated time to travel from source to destination
+#
 def knn_regressor_a_b_lat_long_estimated_time_lookup_table():
     df = pd.read_csv('route_working_mapping.csv')
     X_str = df[['source_destination']].values
@@ -26,13 +27,15 @@ def knn_regressor_a_b_lat_long_estimated_time_lookup_table():
     X = [[float(num) for num in row] for row in X]
     print(X[:10])
 
+    train_len = int(len(X) * 0.8)
     y = df['time_diff'].values
     neigh = KNeighborsRegressor(n_neighbors=5)
-    neigh.fit(X, y)
+    neigh.fit(X[:train_len], y[:train_len])
 
-    print('gt', y[0])
-    print('prediction', neigh.predict([[1.386478068, 103.7602565, 1.387284982, 103.7590052]]))
-
+    # print('gt', y[0])
+    # print('prediction', neigh.predict([[1.386478068, 103.7602565, 1.387284982, 103.7590052]]))
+    print('mean squared error', mean_squared_error(y[train_len:], neigh.predict(X[train_len:])))
+    print('r2 score', r2_score(y[train_len:], neigh.predict(X[train_len:])))
 
 def weather_lookup_table(hourly_time):
     df = pd.read_csv('time_rain.csv').values
@@ -53,7 +56,6 @@ def main():
 
     y = df['time_diff'].values
     # print('y', y[:10])
-    data_len = len(X)
     train_len = int(len(X) * 0.8)
     clf = RandomForestRegressor(max_depth=2, random_state=0)
     clf.fit(X[:train_len], y[:train_len])
