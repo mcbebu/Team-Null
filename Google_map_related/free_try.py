9383936df35958373636da9ac82abc986393494b
AriaPokotengYe <501812381@qq.com>
2023-02-25 21:55:05 +0800

    google map api ready

--- Google_map_related/free_try.py
+++ Google_map_related/free_try.py
@@ -3,7 +3,7 @@ import requests
 import json
 import time
 
-df = pd.read_csv('route_working_mapping.csv')
+df = pd.read_csv('route_mapping_landed.csv')
 df['source_lan'] = df['source_destination'].map(lambda x:x.split(';')[0].split(',')[0])
 df['source_lon'] = df['source_destination'].map(lambda x:x.split(';')[0].split(',')[1])
 df['dest_lan'] = df['source_destination'].map(lambda x:x.split(';')[1].split(',')[0])
@@ -13,20 +13,21 @@ df['google_duration'] = ''
 print(df.head())
 
 count = 0
-key = "AIzaSyBN9wjKeVnXXeMK3dtVWFgFYjGfL18MyyA"
+key = ""
 direction_url = "https://maps.googleapis.com/maps/api/directions/json?origin={},{}&destination={},{}&mode=driving&key={}"
 payload={}
 headers = {}
 for index, row in df.iterrows():
-    print(direction_url.format(row['source_lan'], row['source_lon'], row['dest_lan'], row['dest_lon'], key))
+    # print(direction_url.format(row['source_lan'], row['source_lon'], row['dest_lan'], row['dest_lon'], key))
     response = requests.get(direction_url.format(row['source_lan'], row['source_lon'], row['dest_lan'], row['dest_lon'], key))
     result_json_obj = json.loads(response.text)
-    print(result_json_obj['routes'][0]['legs'][0]['duration']['value'])
+    # print(result_json_obj['routes'][0]['legs'][0]['duration']['value'])
     df.loc[index,'google_duration'] = result_json_obj['routes'][0]['legs'][0]['duration']['value']
-    time.slee(500)
+    time.sleep(0.3)
     count+=1
-    if count>10:
+    if count % 5 == 0:
+        print(count)
+    if count>3000:
         break
-
-
-df.to_csv("route_working_mapping_with_google.csv")
+df = df.drop(['source_lan', 'source_lon', 'dest_lan', 'dest_lon' ], axis=1)
+df.to_csv("route_mapping_landed_with_google.csv")
